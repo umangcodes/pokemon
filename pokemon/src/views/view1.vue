c098a0339a1eeb9b849b0f608f6d123c0572f064
Umang A <umanga.eng@gmail.com>
2021-08-05 19:10:54 -0400

    added view for paging

---
+++ pokemon/src/views/view1.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12" md="4" v-for="data in myCatch" :key="data.id">
+        <Card :pokemon="data" />
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-btn text rounded @click="catchMore()">Go catch more!</v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Card from "@/components/cards/card.vue";
+import { mapState } from "vuex";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Card,
+  },
+  computed: {
+    ...mapState(["myCatch"]),
+  },
+  methods: {
+    catchMore() {
+      console.log("button clicked");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
