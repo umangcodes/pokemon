e1f8c96120c59a2de8e96b9820f3f9450c4e4e0d
Umang A <umanga.eng@gmail.com>
2021-08-06 00:32:44 -0400

    store updated for optimizing api call and data update. varaiblle currentCatch added to track currently available pokemon.

--- pokemon/src/store/index.js
+++ pokemon/src/store/index.js
@@ -8,29 +8,47 @@ export default new Vuex.Store({
     pokemonList: [],
     pokeBallsAvailable: 30,
     currentPage: 1,
+    currentCatch: 0,
     myCatch: [],
+    loadMore: 15,
   },
   mutations: {
     UPDATE_LIST(state, payload) {
-      state.pokemonList = payload.results;
+      state.pokemonList = payload;
+
       // console.log("updated list");
       console.log(state.pokemonList);
+      console.log(state.currentCatch);
     },
     APPEND_LIST(state, payload) {
-      state.myCatch = payload;
-      console.log(state.myCatch);
+      state.myCatch.push(...payload);
+      state.currentCatch = state.myCatch.length;
+      console.log(state.currentCatch);
     },
   },
   actions: {
+    //inital fetch
     async update_list({ state, commit }) {
       let res = await pokemon.catchThemFifty(); // refactor to call the api just once
-      commit("UPDATE_LIST", res.data);
+      commit("UPDATE_LIST", res.data.results);
       let list = [];
       for (let iterator = 0; iterator < state.pokemonList.length; iterator++) {
         let res = await pokemon.catchOne(state.pokemonList[iterator].name);
         list.push(res.data);
       }
       console.log(list);
+      commit("APPEND_LIST", list);
+    },
+    //subsequent fetch using load more button
+    async catchMorePokemons({ state, commit }) {
+      let res = await pokemon.catchThem(state.loadMore, state.currentCatch);
+      state.currentCatch += state.loadMore;
+      commit("UPDATE_LIST", res.data.results);
+      let list = [];
+      for (let iterator = 0; iterator < state.pokemonList.length; iterator++) {
+        let res = await pokemon.catchOne(state.pokemonList[iterator].name);
+        list.push(res.data);
+      }
       commit("APPEND_LIST", list);
     },
   },
