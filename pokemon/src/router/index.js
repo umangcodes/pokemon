3e6195166511202e43faedad973e7ee212a7c82e
Umang A <umanga.eng@gmail.com>
2021-08-09 03:10:14 -0400

    router refactored

--- pokemon/src/router/index.js
+++ pokemon/src/router/index.js
@@ -1,21 +1,35 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-
-import View1 from "@/views/view1.vue";
-import View2 from "@/views/view2.vue";
+import About from "@/views/about.vue";
+import Home from "@/views/home.vue";
+import PokemonsV1 from "@/views/pokemons-v1.vue";
+import PokemonsV2 from "@/views/pokemons-v2.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "view1",
-    component: View1,
+    name: "home",
+    component: Home,
   },
+
   {
-    path: "/view2",
-    name: "view2",
-    component: View2,
+    path: "/about",
+    name: "about",
+    component: About,
+  },
+
+  {
+    path: "/pokemons-v1",
+    name: "pokemons-v1",
+    component: PokemonsV1,
+  },
+
+  {
+    path: "/pokemons-v2/:page?",
+    name: "pokemons-v2",
+    component: PokemonsV2,
   },
 ];
 
@@ -23,6 +37,7 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+  scrollBehavior: () => ({ x: 0, y: 0 }),
 });
 
 export default router;
